a90c45e4f4ad4bb934981847448039a57fc8e3ff
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-04-06 09:37:32 +0700

    bug: increase fieldSeize limit in multer

--- modules/Images.js
+++ modules/Images.js
@@ -33,7 +33,8 @@ const multer = require('multer');
 const upload = multer({
     limits: {
         fileSize: 4 * 1024 * 1024,
-    }
+        fieldSize: 8 * 1024 * 1024
+    },
 });
 
 module.exports = { upload, Resize };
