18bd28d63c3e61b1bdef4f672f50d51def0ac33c
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-04-16 10:06:53 +0700

    refactor: change favicon file path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'resource')));
+app.use(express.static(path.join(__dirname, 'resource', 'favicon')));
 
 app.use(
     session({
