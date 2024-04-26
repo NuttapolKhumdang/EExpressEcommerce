f62ff1e03215f8e4a51d2ba1c409a3bade4fbcab
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-04-26 11:30:29 +0700

    feat: replace stylesheets with tailwind

--- public/javascripts/Alert.js
+++ public/javascripts/Alert.js
@@ -1,6 +1,6 @@
 const AlertType = {
-    ERROR: 'ERROR',
-    INFO: 'INFO',
+    ERROR: "ERROR",
+    INFO: "INFO",
 }
 
 class Alert {
@@ -16,27 +16,31 @@ class Alert {
 
     render() {
         const container = document.createElement("header");
+        const div = document.createElement("div");
         const message = document.createElement("p");
         const button = document.createElement("button");
         const close = document.createElement("span");
 
         close.classList.add("material-icons-outlined")
         close.insertAdjacentHTML("beforeend", "close");
+        button.classList.add("h-8", "w-8", "flex", "items-center", "justify-center", "hover:bg-white", "hover:text-gray-950", "duration-100");
         button.insertAdjacentElement("beforeend", close);
-        message.classList.add("sans");
+        message.classList.add("font-sans", "font-bold");
         message.insertAdjacentHTML("beforeend", this.message);
 
-        container.classList.add("webstatic", "alert-message-container", this.alertType);
-        container.insertAdjacentElement("beforeend", document.createElement("div"));
-        container.insertAdjacentElement("beforeend", message);
-        container.insertAdjacentElement("beforeend", button);
+        div.insertAdjacentElement("beforeend", message);
+        div.insertAdjacentElement("beforeend", button);
+        div.classList.add("grid", "w-full", "max-w-6xl", "grid-cols-[1fr_2rem]", "min-h-8", "place-items-center")
 
+        container.insertAdjacentElement("beforeend", div);
+        container.classList.add("flex", "h-max", "w-full", "items-center", "justify-center", "text-gray-50",this.alertType == 'ERROR' ? 'bg-rose-800' : 'bg-teal-800');
+        console.log(this.alertType);
         document.body.insertAdjacentElement("afterbegin", container);
 
         button.addEventListener("click", () => { this.remove() });
         if (this.alwayon) document.addEventListener("scroll", () => {
-            if (window.scrollY == 0) container.classList.remove("follow");
-            else container.classList.add("follow");
+            if (window.scrollY == 0) container.classList.remove("sticky", "top-16");
+            else container.classList.add("sticky", "top-16");
         });
 
         if (this.timeout !== 0) setTimeout(() => this.remove(), this.timeout);
--- public/javascripts/ProductOptions.js
+++ public/javascripts/ProductOptions.js
@@ -3,29 +3,61 @@ function loadimage(pid, index) {
 }
 
 function createOptionTemplate(pid = "", index = 0, title = "", price = "", optionId = uuidv4(), imageindex = "", imagepreview = null) {
-    return /*html*/`
-        <div class="options" id="${optionId}">
-            <button type="button" onclick="pOptions.removeOption('${optionId}')"><span class="material-icons-outlined">delete</span></button>
-            <section>
-                <header class="mono">ตัวเลือก ${index + 1}</header>
+    //  /*html*/`
+    //     <div class="options" id="${optionId}">
+    //         <button type="button" onclick="pOptions.removeOption('${optionId}')"><span class="material-icons-outlined">delete</span></button>
+    //         <section>
+    //             <header class="mono">ตัวเลือก ${index + 1}</header>
+    //             <menu>
+    //                 ${imageindex
+    //         ? /*html*/`<img src="${imagepreview ?? loadimage(pid, imageindex)}" onclick="selectOptionImage('${optionId}')">`
+    //         : /*html*/`<button type="button" onclick="selectOptionImage('${optionId}')">
+    //                 <span class="material-icons-outlined">art_track</span>
+    //                 <p class="sans">เลือกรูปภาพสินค้า</p>
+    //             </button>`}
+    //             </menu>
+    //         </section>
+    //         <fieldset>
+    //             <input type="hidden" id="imageindex" value="${imageindex}">
+    //             <input type="hidden" id="optionid" value="${optionId}">
+    //             <label for="title" class="mono">ชื่อตัวเลือก</label>
+    //             <input type="text" id="title" class="sans" placeholder="ชื่อตัวเลือก" value="${title ? title : ''}">
+    //             <label for="price" class="mono">ราคา</label>
+    //             <input type="number" id="price" class="sans" placeholder="ราคา" min="0" value="${price ? price : ''}">
+    //         </fieldset >
+    //     </div >`;
+
+    return /*html*/`<div id="${optionId}" class="options relative grid grid-cols-[16rem_1fr] gap-2 border-2 border-gray-950" >
+        <button type="button" onclick="pOptions.removeOption('${optionId}')" class="absolute right-0 top-0 flex h-max w-max items-center justify-center border-b-2 border-l-2 border-gray-950 p-1 hover:bg-gray-950 hover:text-white" >
+          <span class="material-icons-outlined">delete</span>
+        </button>
+
+        <section class="flex flex-col gap-2 p-4">
+            <header class="font-mono text-lg">ตัวเลือก ${index + 1}</header>
                 <menu>
-                    ${imageindex
-            ? /*html*/`<img src="${imagepreview ?? loadimage(pid, imageindex)}" onclick="selectOptionImage('${optionId}')">`
-            : /*html*/`<button type="button" onclick="selectOptionImage('${optionId}')">
-                    <span class="material-icons-outlined">art_track</span>
-                    <p class="sans">เลือกรูปภาพสินค้า</p>
-                </button>`}
+                ${imageindex
+            ? /*html*/` <img onclick="selectOptionImage('{{ option.id }}')" src="${imagepreview ?? loadimage(pid, imageindex)}" loading="lazy" alt="option image preview" class="h-auto max-h-48 w-full object-cover" />`
+            : /*html*/` <button type="button" onclick="selectOptionImage('${optionId}')" class="flex min-h-12 w-full flex-row items-center justify-center gap-2 border-2 border-gray-950 hover:bg-gray-950 hover:text-white">
+                            <span class="material-icons-outlined">art_track</span>
+                            <p class="sans">เลือกรูปภาพสินค้า</p>
+                        </button>`}
                 </menu>
-            </section>
-            <fieldset>
-                <input type="hidden" id="imageindex" value="${imageindex}">
-                <input type="hidden" id="optionid" value="${optionId}">
-                <label for="title" class="mono">ชื่อตัวเลือก</label>
-                <input type="text" id="title" class="sans" placeholder="ชื่อตัวเลือก" value="${title ? title : ''}">
-                <label for="price" class="mono">ราคา</label>
-                <input type="number" id="price" class="sans" placeholder="ราคา" min="0" value="${price ? price : ''}">
-            </fieldset >
-        </div >`;
+        </section>
+        <fieldset class="flex h-full flex-col gap-4 border-l-2 border-gray-950 p-4">
+            <input type="hidden" id="imageindex" value="${imageindex}">
+            <input type="hidden" id="optionid" value="${optionId}">
+          
+            <label for="title" class="grid grid-cols-1 gap-2">
+                <span class="w-max font-mono text-sm underline">ชื่อสินค้า</span>
+                <input type="text" id="title" placeholder="ชื่อตัวเลือก" value="${title ? title : ''}" class="h-12 border-b-2 border-gray-950 py-4 font-sans outline-offset-4" />
+            </label>
+
+            <label for="price" class="grid grid-cols-1 gap-2">
+                <span class="w-max font-mono text-sm underline">ราคา</span>
+                <input type="number" id="price" placeholder="ราคา" min="0" value="${price ? price : ''}" class="h-12 border-b-2 border-gray-950 py-4 font-sans outline-offset-4" />
+            </label>
+        </fieldset>
+    </div>`;
 }
 
 class ProductOptions {
