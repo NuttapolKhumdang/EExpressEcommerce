4a701caafb79b7ca186d418d85bb44561ff0145e
NuttapolKhumdang <nuttapol.chok@gmail.com>
2024-05-13 17:25:06 +0700

    ci: update query result on query and replace content map with module

--- routes/articles.js
+++ routes/articles.js
@@ -11,9 +11,10 @@ const parseSearch = require('../modules/ParseSearch');
 
 const Access = require('../modules/Access');
 const { Rights, AccountRole } = require('../modules/AccessRights');
+const { articleMapContent } = require('../modules/Utils');
 
 router.get('/', async (req, res, next) => {
-    const article = await Article.find({ public: true });
+    const article = await Article.find({ forproduct: { $exists: false }, public: true });
     return res.render('articles', { article });
 });
 
@@ -32,7 +33,7 @@ router.route('/writer/:id')
     .get(Access(Rights.ARTICLE.WRITE), async (req, res, next) => {
         try {
             const article = await Article.findById(req.params.id);
-            return res.render('writer', { title: article.title, article });
+            return res.render('writer', { account: req?.user, title: article.title, article });
         } catch (e) { console.error(e); return next('route') };
     })
     .put(Access(Rights.ARTICLE.WRITE, true), async (req, res, next) => {
@@ -69,40 +70,9 @@ router.get('/static/:search', async (req, res, next) => {
 
 router.get('/:search', async (req, res, next) => {
     try {
-        const article = await Article.findOne({ search: req.params.search, deleted: false });
+        const article = await Article.findOne({ search: req.params.search, forproduct: { $exists: false }, deleted: false });
         if (!article || article.length == 0) return next('route');
-        const content = article.content.map(e => {
-            if (e.text.startsWith("[TABLE]")) {
-                let table = [];
-                let tname = "";
-
-                for (let l of e.text.split("\n")) {
-                    let r, tag, rawChild;
-
-                    if (l.startsWith("[TABLE]")) {
-                        tname = l.split("[TABLE]")[1];
-                        continue;
-                    }
-
-                    if (l.startsWith("[HEAD]")) {
-                        rawChild = l.split("[HEAD]")[1].split(",");
-                        tag = 'TH';
-                    }
-                    else {
-                        rawChild = l.split(",");
-                        tag = 'TD';
-                    }
-
-                    if (!tag) continue;
-                    r = {
-                        tag,
-                        child: rawChild.map(e => e.trim()),
-                    }
-                    table.push(r);
-                }
-                return { tag: 'TABLE', table, tname };
-            } else return e;
-        });
+        const content = articleMapContent(article?.content);
 
         return res.render('article', { render: 'articles/reader.html', title: article.title, article, content });
     } catch (e) {
